c17a20c46bd9553ac20f6e79ac74c8661646276f
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2024-04-05 09:03:05 +0530

    Added the middleware functions

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,47 +1,27 @@
-console.log('Express Tutorial Learning')
+const express = require('express')
+const logger = require('./logger')
+const authorize = require('./authorize')
+const app = express();
 
-const http = require('http')
-const {readFileSync} = require('fs')
-// get all files
-const homePage  = readFileSync('./navbar-app/index.html')
-const homeStyles  = readFileSync('./navbar-app/styles.css')
-const homeImage  = readFileSync('./navbar-app/logo.svg')
-const homeLogic  = readFileSync('./navbar-app/browser-app.js')
+app.use([logger,  authorize])    
 
+app.get('/', (req, res) => {
+    console.log(req.user  )
+    res.send('Home')
+})
 
-console.log(homePage)
+app.get('/about', (req, res) => {
+    res.send('About')
+})
 
-const server = http.createServer((req, res) => {
-    console.log('User hit the server', req.url)
-    const url = req.url;
-    if (url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write(homePage)
-        res.end()
-    }
-     else if (url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>About Page</h1')
-        res.end()
-    }
-    else if (url === '/styles.css') {
-        res.writeHead(200, {'content-type': 'text/css'})
-        res.write(homeStyles)
-        res.end()
-    }else if (url === '/logo.svg') {
-        res.writeHead(200, {'content-type': 'image/svg+xml'})
-        res.write(homeImage)
-        res.end()
-    } else if (url === '/browser-app.js') {
-        res.writeHead(200, {'content-type': 'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-    } 
-    else {
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.write('<h1>404 Page Not Found</h1')
-        res.end()
-    }
- })
+app.get('/api/products', (req, res) => {
+    res.send('products')
+})
 
-server.listen(1000) 
+app.get('/api/items', (req, res) => {
+    res.send('items')
+})
+
+app.listen(1000, () => {
+    console.log('Server is listening to  the port 1000')
+})
